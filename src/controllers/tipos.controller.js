61f6b9e1ddc443bcf22faa55b1ae7b4449f905b8
madara <wcamacho221@gmail.com>
2021-03-19 04:22:10 -0500

    Modulos Filtros Agregados

--- src/controllers/tipos.controller.js
+++ src/controllers/tipos.controller.js
@@ -1,20 +1,56 @@
 import pool from '../database'
 
+
+
 export const getTipos = async(req,res) =>{
-    const datos = await pool.query('SELECT DISTINCT marca,largo,diametro FROM productos')
-    res.json(datos)
+    const datosMarca = await pool.query('SELECT DISTINCT marca FROM productos')
+    const datosLargo = await pool.query('SELECT DISTINCT largo FROM productos')
+    const datosDiametro = await pool.query('SELECT DISTINCT diametro FROM productos')
+    res.json({marca:datosMarca, largo:datosLargo, diametro:datosDiametro})
+    
 }
 
 export const getTiposByCategorias = async(req,res) =>{
     const { id } = req.params;
-    const datos = await pool.query('SELECT DISTINCT marca,largo,diametro FROM productos WHERE fk_categorias =?',[id])
-    res.json(datos)
+    const datosMarca = await pool.query('SELECT DISTINCT marca FROM productos WHERE fk_categorias =?',[id])	
+    const datosLargo = await pool.query('SELECT DISTINCT largo FROM productos WHERE fk_categorias =?',[id])	
+    const datosDiametro = await pool.query('SELECT DISTINCT diametro FROM productos WHERE fk_categorias =?',[id])
+
+		res.json({marca:datosMarca, largo:datosLargo, diametro:datosDiametro})
 }
 
 
 export const getFilterCategories = async(req,res) =>{
     const { id } = req.params;
-    const { filter } = req.body
-    const datos = await pool.query('SELECT * FROM productos WHERE (marca = ? OR largo = ? OR diametro = ?) AND fk_categorias = ?',[filter,filter,filter,id])
-    res.json(datos)
+		const { filter_diametro, filter_marca, filter_largo } = req.query;
+    if(filter_diametro !== undefined && filter_marca !== undefined && filter_largo !== undefined){
+      const datos = await pool.query('SELECT * FROM productos WHERE (marca = ? OR largo = ? OR diametro = ?) AND fk_categorias = ?',[filter_marca,filter_largo, filter_diametro,id])
+      return res.json(datos)
+
+    }else if (filter_diametro !== undefined && filter_marca !== undefined){
+      const datos = await pool.query('SELECT * FROM productos WHERE (marca = ? OR diametro = ?) AND fk_categorias = ?',[filter_marca,filter_diametro,id])
+      return res.json(datos)
+    
+    }else if(filter_diametro !== undefined && filter_largo !== undefined){
+      const datos = await pool.query('SELECT * FROM productos WHERE (diametro = ? OR largo = ?) AND fk_categorias = ?',[filter_diametro,filter_largo,id])
+      return res.json(datos)
+    
+    }else if(filter_marca !== undefined && filter_largo !== undefined){
+      const datos = await pool.query('SELECT * FROM productos WHERE (marca = ? OR largo = ?) AND fk_categorias = ?',[filter_marca,filter_largo,id])
+      return res.json(datos)
+    
+    }else if(filter_diametro !== undefined){
+      const datos = await pool.query('SELECT * FROM productos WHERE diametro = ? AND fk_categorias = ?',[filter_diametro,id])
+      return res.json(datos)
+    
+    }else if(filter_largo !== undefined){
+      const datos = await pool.query('SELECT * FROM productos WHERE largo = ? AND fk_categorias = ?',[filter_largo,id])
+      return res.json(datos)
+    
+    }else if(filter_marca !== undefined){
+      const datos = await pool.query('SELECT * FROM productos WHERE marca = ? AND fk_categorias = ?',[filter_marca,id])
+      return res.json(datos)
+    }
+
+	res.json({error:'xd'})
 }
