2f3155d9d94867bb288fa8f02cc643484e6bb91f
madara <wcamacho221@gmail.com>
2021-03-20 03:15:01 -0500

    Bug fix

--- src/controllers/tipos.controller.js
+++ src/controllers/tipos.controller.js
@@ -23,34 +23,11 @@ export const getTiposByCategorias = async(req,res) =>{
 export const getFilterCategories = async(req,res) =>{
     const { id } = req.params;
 		const { filter_diametro, filter_marca, filter_largo } = req.query;
-    if(filter_diametro !== undefined && filter_marca !== undefined && filter_largo !== undefined){
-      const datos = await pool.query('SELECT * FROM productos WHERE (marca = ? OR largo = ? OR diametro = ?) AND fk_categorias = ?',[filter_marca,filter_largo, filter_diametro,id])
-      return res.json(datos)
+    if(filter_diametro !== undefined || filter_marca !== undefined || filter_largo !== undefined){
+      const datos = await pool.query('SELECT * FROM productos WHERE (marca = ? OR largo = ? OR diametro = ?) AND  fk_categorias = ?',[filter_marca,filter_largo,filter_diametro,id])
+      res.json(datos)
+    }else{
+		res.json({error:'xd'})
+		}
 
-    }else if (filter_diametro !== undefined && filter_marca !== undefined){
-      const datos = await pool.query('SELECT * FROM productos WHERE (marca = ? OR diametro = ?) AND fk_categorias = ?',[filter_marca,filter_diametro,id])
-      return res.json(datos)
-    
-    }else if(filter_diametro !== undefined && filter_largo !== undefined){
-      const datos = await pool.query('SELECT * FROM productos WHERE (diametro = ? OR largo = ?) AND fk_categorias = ?',[filter_diametro,filter_largo,id])
-      return res.json(datos)
-    
-    }else if(filter_marca !== undefined && filter_largo !== undefined){
-      const datos = await pool.query('SELECT * FROM productos WHERE (marca = ? OR largo = ?) AND fk_categorias = ?',[filter_marca,filter_largo,id])
-      return res.json(datos)
-    
-    }else if(filter_diametro !== undefined){
-      const datos = await pool.query('SELECT * FROM productos WHERE diametro = ? AND fk_categorias = ?',[filter_diametro,id])
-      return res.json(datos)
-    
-    }else if(filter_largo !== undefined){
-      const datos = await pool.query('SELECT * FROM productos WHERE largo = ? AND fk_categorias = ?',[filter_largo,id])
-      return res.json(datos)
-    
-    }else if(filter_marca !== undefined){
-      const datos = await pool.query('SELECT * FROM productos WHERE marca = ? AND fk_categorias = ?',[filter_marca,id])
-      return res.json(datos)
-    }
-
-	res.json({error:'xd'})
 }
