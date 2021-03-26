1fe8bf8fc5a57ce800c2db912d554a4ed147633c
madara <wcamacho221@gmail.com>
2021-03-26 11:55:10 -0500

    Package arreglado

--- src/key.js
+++ src/key.js
@@ -1,6 +1,6 @@
 export const database = {
-    host: 'localhost',
-    user: 'madara',
-    password:'password',
-    database: 'tornillos'
+    host: '37.153.91.241',
+    user: 'tornillo_zukytech',
+    password:'Zuky317@',
+    database: 'tornillo_db'
 }
